1a64c58247394daf064e76e0aee095000bf3803b
Benjamin Horsleben <benjamin@fizkerinc.dk>
2022-04-23 00:39:01 +0200

    Updated the stripUTF8BOM() function to allow passing in either arrays or straight strings

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import { promises as fs } from "fs"
 
-export async function stripUTF8BOM(filenames) {
-	const tasks = filenames.map(filename => async () => {
+export async function stripUTF8BOM(...filenames) {
+	const tasks = filenames.flat().map(filename => async () => {
 		let content = await fs.readFile(filename)
 		if(content[0] === 239 && content[1] === 187 && content[2] === 191) {
 			content = content.slice(3)
