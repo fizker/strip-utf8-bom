8a7711e6d76ebae1a15067093b536256df17b983
Benjamin Horsleben <benjamin@fizkerinc.dk>
2022-05-08 11:52:02 +0200

    Simplified the main function to not produce an array of promise-factories and instead rely on await

--- index.mjs
+++ index.mjs
@@ -1,13 +1,11 @@
 import { promises as fs } from "fs"
 
 export async function stripUTF8BOM(...filenames) {
-	const tasks = filenames.flat().map(filename => async () => {
+	for(const filename of filenames.flat()) {
 		let content = await fs.readFile(filename)
 		if(content[0] === 239 && content[1] === 187 && content[2] === 191) {
 			content = content.slice(3)
 		}
 		await fs.writeFile(filename, content)
-	})
-
-	await tasks.reduce((p, t) => p.then(t), Promise.resolve())
+	}
 }
