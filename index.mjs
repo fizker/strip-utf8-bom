f71d9cd14624aa2612233936d2d006cf57207202
Benjamin Horsleben <benjamin@fizkerinc.dk>
2022-05-08 12:11:22 +0200

    Optimized the flow by only reading the entire file if the first three bytes match the BOM

--- index.mjs
+++ index.mjs
@@ -2,10 +2,18 @@ import { promises as fs } from "fs"
 
 export async function stripUTF8BOM(...filenames) {
 	for(const filename of filenames.flat()) {
-		let content = await fs.readFile(filename)
-		if(content[0] === 239 && content[1] === 187 && content[2] === 191) {
-			content = content.slice(3)
+		const filehandle = await fs.open(filename, "r")
+		const { buffer } = await filehandle.read({
+			buffer: Buffer.alloc(3),
+		})
+
+		const didFindBOM = buffer[0] === 239 && buffer[1] === 187 && buffer[2] === 191
+		if(!didFindBOM) {
+			continue
 		}
+
+		const content = await filehandle.readFile({ encoding: "utf8" })
+		await filehandle.close()
 		await fs.writeFile(filename, content)
 	}
 }
